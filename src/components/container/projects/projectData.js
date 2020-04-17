dbeb8996aa0a81b91bcf7acdd0362dc405f7f38f
Tajamul-Husen <tajamulhusen58@gmail.com>
2020-04-17 17:22:29 +0530

    one project added

--- src/components/container/projects/projectData.js
+++ src/components/container/projects/projectData.js
@@ -2,31 +2,38 @@ const img1 = require("../../../assets/img/projects/project_1.jpg");
 const img2 = require("../../../assets/img/projects/project_2.jpg");
 const img3 = require("../../../assets/img/projects/project_3.jpg");
 const img4 = require("../../../assets/img/projects/project_4.jpg");
+const img5 = require("../../../assets/img/projects/project_5.jpg");
 
 const projectData = [
   {
     projectImg: img1,
     altName: "hosting website",
-    preview: "https://filehost.netlify.com/",
+    preview: "https://filehost.netlify.app/",
     github: "https://github.com/Tajamul-Husen/FileHost"
   },
   {
     projectImg: img2,
     altName: "blog website",
-    preview: "https://livinglifesimple.netlify.com/",
+    preview: "https://livinglifesimple.netlify.app/",
     github: "https://github.com/Tajamul-Husen/A_Multipage_Blog"
   },
   {
     projectImg: img3,
     altName: "shopping website",
-    preview: "https://shoeclues.netlify.com/",
+    preview: "https://shoeclues.netlify.app/",
     github: "https://github.com/Tajamul-Husen/shopping_cart"
   },
   {
     projectImg: img4,
     altName: "game application",
-    preview: "https://pokemon-mem-game.netlify.com/",
+    preview: "https://pokemon-mem-game.netlify.app/",
     github:"https://github.com/Tajamul-Husen/poke_memory_game"
+  },
+  {
+    projectImg: img5,
+    altName: "corona tracker",
+    preview: "https://corona-project.netlify.app/",
+    github:"https://github.com/Tajamul-Husen/corona-project"
   }
 ];
 
