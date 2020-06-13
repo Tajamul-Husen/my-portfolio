ef45855f3e7a19f7585f49aa87e509adfbab842d
Tajamul-Husen <tajamulhusen58@gmail.com>
2020-06-13 22:51:03 +0530

    removed credentials

--- src/components/container/contact/contactData.js
+++ src/components/container/contact/contactData.js
@@ -1,30 +1,22 @@
-import {
-  faMapMarkerAlt,
-  faEnvelope,
-  faMobileAlt
-} from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 const contactData = [
   {
     icon: faMapMarkerAlt,
-    contact: "Byculla(W), Mumbai, India."
+    contact: "Byculla(W), Mumbai, India.",
   },
   {
     icon: faEnvelope,
-    contact: "tajamulhusen58@gmail.com"
-  },
-  {
-    icon: faMobileAlt,
-    contact: "+91 7738229408"
+    contact: "tajamulhusen58@gmail.com",
   },
   {
     icon: faGithub,
-    contact: "github.com/Tajamul-Husen"
+    contact: "github.com/Tajamul-Husen",
   },
   {
     icon: faLinkedinIn,
-    contact: "linkedin.com/in/tajamul-husen"
-  }
+    contact: "linkedin.com/in/tajamul-husen",
+  },
 ];
 
 export default contactData;
